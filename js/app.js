af67dee7b8e7b8faea3d7eff3fed4376f0e7ba1b
Francesca-Barletta <wallebar@gmail.com>
2024-03-05 14:54:52 +0100

    add pseudo-code

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('form biglietto treno');
+
+// prendere i dati che l'utente inserisce tramite il form
+//il primo dato sono i km del viaggio
+//il secondo è l'età del passeggero
+//sulla base di questo calcolare il prezzo del biglietto in base a
+    //prezzo base che è dato da il numero dei km per 0.21
+    //il prezzo scontato del 20% se età passeggero minorenne
+    //il prezzo scontato del 40% se età passeggero over 65
+//quando l'utente clicca invia noi recuperiamo i value di queste 2 input
+//il prezzo finale va stampato in console log
